e3ed0bcc7ae3b0896055c1665439d7f36f43d413
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-02-23 21:35:43 +0000

    feat(checkIns): Fetch user history of check-ins Possible to fetch user history of chheck-ins. Create use case ,repo and test's

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,6 +2,7 @@ import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
   // we didn't use the checkInCreateInput because it creates the relations instead of use some already created
-  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }
