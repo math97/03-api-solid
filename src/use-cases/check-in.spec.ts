c2455af8894b7a7372ca8e9115b0f9891ed5b2b5
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-02-23 21:15:19 +0000

    test(check-ins): Update test Updat test of check-ins with specific error messages and the new function of create gym

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -3,24 +3,26 @@ import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from '@/use-cases/check-in'
 import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
 import { Decimal } from '@prisma/client/runtime/library'
+import { MaxDistanceError } from './errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error'
 
 let gymsRepository: InMemoryGymsRepository
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     gymsRepository = new InMemoryGymsRepository()
     sut = new CheckInUseCase(checkInsRepository, gymsRepository)
 
-    gymsRepository.gyms.push({
+    await gymsRepository.create({
       id: 'gym-01',
       title: 'JavaScript Gym',
       description: '',
       phone: '',
-      latitude: new Decimal(38.7244881),
-      longitude: new Decimal(-9.1913004),
+      latitude: 38.7244881,
+      longitude: -9.1913004,
     })
 
     vi.useFakeTimers()
@@ -44,12 +46,12 @@ describe('Check-in Use Case', () => {
   it('should not be able to check in on distant gym', async () => {
     await expect(() =>
       sut.execute({
-        gymId: 'gym-02',
+        gymId: 'gym-01',
         userId: 'user-01',
         userLatitude: 38.9232908,
         userLongitude: -9.128977,
       }),
-    ).rejects.toBeInstanceOf(Error)
+    ).rejects.toBeInstanceOf(MaxDistanceError)
   })
 
   it('should not be able to check in twice in the same day', async () => {
@@ -69,7 +71,7 @@ describe('Check-in Use Case', () => {
         userLatitude: 38.7244881,
         userLongitude: -9.1913004,
       }),
-    ).rejects.toBeInstanceOf(Error)
+    ).rejects.toBeInstanceOf(MaxNumberOfCheckInsError)
   })
 
   it('should be able to check in twice but in different days', async () => {
