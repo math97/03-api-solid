e89a1be84733a774e6febe1698b2f14dcd87789f
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-02-27 20:39:23 +0000

    feat(gyms): Create controllers and routes Create controllers for nearby and search use case. Also route for both

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { nearby } from './nearby'
+import { search } from './search'
+import { create } from './create'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
