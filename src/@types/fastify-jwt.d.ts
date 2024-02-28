aeba4fd0eaa13e21c68ee8505b2120610c489111
Matheus DEV <matheusdev@L-LLQPKP2GFG.local>
2024-02-28 19:37:38 +0000

    feat(user): Add refresh token using RBCA and add role to user

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -4,6 +4,7 @@ import '@fastify/jwt'
 export declare module '@fastify/jwt' {
   interface FastifyJWT {
     user: {
+      role: 'ADMIN' | 'MEMBER'
       sub: string
     } // user type is return type of `request.user` object
   }
